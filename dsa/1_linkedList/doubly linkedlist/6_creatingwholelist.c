5a394334528ad91081d0f1a6d7058ee4ff8339bd
MD AFTAB <ibaftab.code@gmail.com>
2026-08-27 16:03:56 +0530

    print optimize

--- dsa/1_linkedList/doubly linkedlist/6_creatingwholelist.c
+++ dsa/1_linkedList/doubly linkedlist/6_creatingwholelist.c
@@ -39,7 +39,7 @@
 
     node *createlist(node *head){
         int data, n;
-        printf("enter the number of node :");
+        printf("enter the number node :");
         scanf("%d", &n);
 
         if(n == 0) return head;
